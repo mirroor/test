14c5bb688109f189f9c25ba3c764636471429d62
mirrors <164948826@qq.com>
2020-02-18 12:41:35 +0800

    v2.0

--- src/A.java
+++ src/A.java
@@ -1,9 +1,8 @@
-package PACKAGE_NAME;
 
-/**
- * @author weijh
- * @date 2/18/2020
- */
 public class A
 {
+    public static void main(String[] args)
+    {
+        System.out.println("A");
+    }
 }
