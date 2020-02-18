357de343471585f78de7f13b755659f036502bc5
mirrors <164948826@qq.com>
2020-02-18 20:46:45 +0800

    bbbb

--- src/B.java
+++ src/B.java
@@ -3,10 +3,6 @@ public class B
 {
     public static void main(String[] args)
     {
-<<<<<<< HEAD
-        System.out.println("merge");
-=======
-        System.out.println("aa");
->>>>>>> a
+        System.out.println("BBBB");
     }
 }
